56ac8ac60b9d53837f080937e0efb4c59115afc3
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-14 20:15:53 -0700

    build test data
    currently will not load

--- api_v1/models.py
+++ api_v1/models.py
@@ -5,8 +5,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-
-# Create your models here.
 class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.TextField()
