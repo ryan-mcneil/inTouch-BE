214f107e7d1276ac7cb82cc398a7a4a08d12c1c4
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-14 17:10:13 -0700

    add model definition

--- api_v1/models.py
+++ api_v1/models.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField()
+    frequency = models.IntegerField(default=10)
+    priority = models.IntegerField(default=3)
+    next_reminder = models.DateField(timezone.now)
+    last_contacted = models.DateField(timezone.now)
+    notes = models.TextField(default="")
