4102f389d2bbc650ac023b3208a8b65ec8173093
William Fischer <william.fischer42@gmail.com>
2019-02-17 09:35:56 -0700

    Configure procfile & requirements

--- in_touch_be/settings.py
+++ in_touch_be/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -77,7 +76,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'in_touch_be.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
