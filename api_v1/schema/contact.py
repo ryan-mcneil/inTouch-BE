a3608ee69165e462f2b84f42de86e9c07592312d
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-16 19:10:26 -0700

    UpdateContact method
    works for string and int fields. need to determine how to sent date in request

--- api_v1/schema/contact.py
+++ api_v1/schema/contact.py
@@ -43,7 +43,7 @@ class CreateContact(graphene.Mutation):
         user = info.context.user
         ok = True
         contact_instance = Contact(
-            id = input.id,
+            # id = input.id,
             name = input.name,
             frequency = input.frequency,
             priority = input.priority,
@@ -55,5 +55,53 @@ class CreateContact(graphene.Mutation):
         contact_instance.save()
         return CreateContact(ok=ok, contact=contact_instance)
 
+class UpdateContact(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = ContactInput(required=True)
+
+    ok = graphene.Boolean()
+    contact = graphene.Field(ContactType)
+
+    @staticmethod
+    @login_required
+    def mutate(root, info, id, input=None):
+        user = info.context.user
+        ok = False
+        contact_instance = Contact.objects.get(pk=id)
+        if contact_instance and user.id == contact_instance.user_id:
+            ok = True
+            # add the following once implemented
+            # contact_details = []
+            # occasions = []
+
+            # import code; code.interact(local=dict(globals(), **locals()))
+            # for key in input:
+            #     contact_instance[key] = input[key]
+            #     contact_instance.key = input[key]
+
+            if input.name:
+                contact_instance.name = input.name
+
+            if input.frequency:
+                contact_instance.frequency = input.frequency
+
+            if input.priority:
+                contact_instance.priority = input.priority
+
+            if input.next_reminder:
+                contact_instance.next_reminder = input.next_reminder
+
+            if input.last_contacted:
+                contact_instance.last_contacted = input.last_contacted
+
+            if input.notes:
+                contact_instance.notes = input.notes
+
+            contact_instance.save()
+            return UpdateContact(ok=ok, contact=contact_instance)
+        return UpdateContact(ok=ok, contact=None)
+
 class Mutation(graphene.ObjectType):
     create_contact = CreateContact.Field()
+    update_contact = UpdateContact.Field()
